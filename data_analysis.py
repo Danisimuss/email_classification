db737b3dc95aea54caa35ce5aaf8b3f75a927873
Danisimuss <99678327+Danisimuss@users.noreply.github.com>
2026-06-02 00:15:01 +0300

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 def analysis(category_stats):
-        categories = ['Черновик', 'Спам', 'Важное', 'Уведомления', 'Работа', "Ошибка файла", "Неотсортированное"]
+        categories = ['Черновик', 'Спам', 'Важное', 'Уведомления', 'Работа', "Ошибка файла", "Несортированное"]
         colors = ['#ff9999', '#66b3ff', '#99ff99', '#ffcc99']
         plt.figure(figsize=(7, 7))
         plt.pie(category_stats,
