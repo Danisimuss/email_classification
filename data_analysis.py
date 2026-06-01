6bc26cbea2688569d7d81cba55bbcb4b37be214e
Danisimuss <99678327+Danisimuss@users.noreply.github.com>
2026-06-01 23:05:46 +0300

    feat: add data_analysis.py

---
+++ data_analysis.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+def analysis(category_stats):
+        categories = ['Черновик', 'Спам', 'Важное', 'Уведомления', 'Работа', "Ошибка файла", "Неотсортированное"]
+        colors = ['#ff9999', '#66b3ff', '#99ff99', '#ffcc99']
+        plt.figure(figsize=(7, 7))
+        plt.pie(category_stats,
+                labels=categories,
+                colors=colors,
+                autopct='%1.1f%%',
+                startangle=140)
+        plt.title('Распределение писем по категориям')
+        plt.savefig('email_analysis.png', dpi=300, bbox_inches='tight')
+        plt.show()
